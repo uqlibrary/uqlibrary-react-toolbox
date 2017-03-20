30cf83f1abf9b5908aabe4190677ffaee5f7ac0e
uqadient <a.diente@library.uq.edu.au>
2017-03-21 09:11:03 +1000

    updated reference to HelpIcon and helpDrawer export

--- src/HelpDrawer/index.js
+++ src/HelpDrawer/index.js
@@ -1,4 +1,4 @@
-export default from './containers/HelpIcon';
-export default from './containers/HelpDawer';
+export {default as HelpIcon} from './containers/HelpIcon';
+export {default as HelpDrawer} from './containers/HelpDawer';
 
 export {default as reducer} from './reducer';
