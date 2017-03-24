7642de7c029e2a684ec8b671a62f68dff7cd4b47
uqadient <a.diente@library.uq.edu.au>
2017-03-24 15:43:42 +1000

    added basic snapshot testing and readme.md

---
+++ src/AutoCompleteSelect/AsyncAutoCompleteSelectWrapper.snapshot.test.js
@@ -0,0 +1,40 @@
+jest.dontMock('./AsyncAutoCompleteSelectWrapper');
+
+import {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json';
+import React from 'react';
+import AsyncAutoCompleteSelectWrapper from './AsyncAutoCompleteSelectWrapper';
+import filterProps from '../helpers/_filterProps';
+
+function setup(props) {
+    const consolidatedProps = filterProps(props, AsyncAutoCompleteSelectWrapper.propTypes);
+    consolidatedProps.onTouchTap = props.input.onBlur;
+    consolidatedProps.error = !!consolidatedProps.errorText;
+
+    return shallow(<AsyncAutoCompleteSelectWrapper {...props} />);
+}
+
+describe('AsyncAutoCompleteSelectWrapper snapshots tests', () => {
+    it('renders asyncautocomplete field', () => {
+        const props =
+            {
+                name: 'owner',
+                label: 'Owner',
+                disabled: false,
+                filterItems: jest.fn(),
+                labelField: 'name',
+                value: {},
+                errorText: '',
+                meta: {
+                    touched: null
+                },
+                input: {
+                    onBlur: null
+                }
+            };
+
+        const wrapper = setup(props);
+        const tree = toJson(wrapper);
+        expect(tree).toMatchSnapshot();
+    });
+});
---
+++ src/AutoCompleteSelect/AutoCompleteSelectWrapper.snapshot.test.js
@@ -0,0 +1,40 @@
+jest.dontMock('./AutoCompleteSelectWrapper');
+
+import {shallow} from 'enzyme';
+import toJson from 'enzyme-to-json';
+import React from 'react';
+import AutoCompleteSelectWrapper from './AutoCompleteSelectWrapper';
+import filterProps from '../helpers/_filterProps';
+
+function setup(props) {
+    const consolidatedProps = filterProps(props, AutoCompleteSelectWrapper.propTypes);
+    consolidatedProps.onTouchTap = props.input.onBlur;
+    consolidatedProps.error = !!consolidatedProps.errorText;
+
+    return shallow(<AutoCompleteSelectWrapper {...props} />);
+}
+
+describe('AutoCompleteSelectWrapper snapshots tests', () => {
+    it('renders autocomplete field', () => {
+        const props =
+            {
+                name: 'owner',
+                label: 'Owner',
+                disabled: false,
+                filterItems: jest.fn(),
+                labelField: 'name',
+                value: {},
+                errorText: '',
+                meta: {
+                    touched: null
+                },
+                input: {
+                    onBlur: null
+                }
+            };
+
+        const wrapper = setup(props);
+        const tree = toJson(wrapper);
+        expect(tree).toMatchSnapshot();
+    });
+});
