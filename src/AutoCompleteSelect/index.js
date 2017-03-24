4d5948a098379c712b3bbfe00bec9d9879f72eb2
uqadient <a.diente@library.uq.edu.au>
2017-03-24 16:40:00 +1000

    added index.js file to export the components

---
+++ src/AutoCompleteSelect/index.js
@@ -0,0 +1,4 @@
+export {default as AutoCompleteSelectWrapper} from './AutoCompleteSelectWrapper';
+export {default as AsyncAutoCompleteSelectWrapper} from './AsyncAutoCompleteSelectWrapper';
+export {default as AutoCompleteSelect} from './AutoCompleteSelect';
+export {default as AsyncAutoCompleteSelect} from './AsyncAutoCompleteSelect';
