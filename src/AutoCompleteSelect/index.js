1ea469ef5821a63996374a83965c6320aa370b56
uqadient <a.diente@library.uq.edu.au>
2017-03-27 08:35:57 +1000

    altered export and fixed typo

--- src/AutoCompleteSelect/index.js
+++ src/AutoCompleteSelect/index.js
@@ -1,4 +1,4 @@
 export {default as AutoCompleteSelectWrapper} from './AutoCompleteSelect';
 export {default as AsyncAutoCompleteSelectWrapper} from './AsyncAutoCompleteSelect';
-export {default as AutoCompleteSelect} from './AutoCompleteSelect/index';
-export {default as AsyncAutoCompleteSelect} from './AutoCompleteSelect/inex';
+export {AutoCompleteSelect} from './AutoCompleteSelect/index';
+export {AsyncAutoCompleteSelect} from './AutoCompleteSelect/index';
