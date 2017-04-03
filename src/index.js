05cebb2790f2f9633f53d2c59114e9073ef88f39
uqadient <a.diente@library.uq.edu.au>
2017-04-04 08:13:33 +1000

    export the TextField component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export {AutoCompleteSelect, AsyncAutoCompleteSelect} from './AutoCompleteSelect';
 export {HelpIcon, HelpDrawer, reducer} from './HelpDrawer';
 export {MenuDrawer} from './MenuDrawer';
+export {TextField} from './TextField';
 export {gaComponent} from './GA';
