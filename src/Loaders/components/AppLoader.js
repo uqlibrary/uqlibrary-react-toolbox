26280d8bc00a3b9b265cce0b0ed3b752eeef0538
Ky <k.lane@uq.edu.au>
2017-04-26 16:23:33 +1000

    Fixing up app loader to new grid CSS

--- src/Loaders/components/AppLoader.js
+++ src/Loaders/components/AppLoader.js
@@ -4,14 +4,16 @@ import CircularProgress from 'material-ui/CircularProgress';
 
 export default function AppLoader({title, logoImage, logoText, progressColor}) {
     return (
-        <div className="app-loader column layout-fill align-center justify-center">
-            <h1 className="display-2">{title}</h1>
-            <br />
-            <br />
-            <CircularProgress size={80} thickness={8} color={progressColor} />
-            <br />
-            <br />
-            {logoImage && <img src={logoImage} alt={logoText} />}
+        <div className="app-loader" >
+            <div className="app-loader-content">
+                <h1 className="display-2">{title}</h1>
+                <br />
+                <br />
+                <CircularProgress size={80} thickness={8} color={progressColor} />
+                <br />
+                <br />
+                {logoImage && <img src={logoImage} alt={logoText} />}
+            </div>
         </div>
     );
 }
