a851b708a02931ee3bc59bf5523cfd3e867d486d
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2017-05-16 13:04:48 +1000

    Fix enzyme errors and remove styles from helpicon

--- src/HelpDrawer/components/HelpIcon.js
+++ src/HelpDrawer/components/HelpIcon.js
@@ -10,30 +10,24 @@ const propTypes = {
     tooltip: PropTypes.string,
     tooltipIconColor: PropTypes.string,
     onClick: PropTypes.func,
-    inline: PropTypes.bool,
     style: PropTypes.object
 };
 
 const defaultProps = {
-    inline: false,
     style: {},
     tooltip: 'Click for more information',
     tooltipIconColor: '#CCCCCC'
 };
 
-const HelpIcon = ({title, text, buttonLabel, tooltip, tooltipIconColor, onClick, inline, style}) => {
+const HelpIcon = ({title, text, buttonLabel, tooltip, tooltipIconColor, onClick, style}) => {
     const setDrawerContent = () => {
         onClick(title, text, buttonLabel);
     };
 
-    const classNames = `form-help-icon is-narrow is-pulled right ${inline ? 'inline' : ''}`;
-
     return (
-        <div className={classNames} style={{...style}}>
-            <IconButton tooltip={tooltip} tooltipPosition="bottom-left" onClick={setDrawerContent} className="is-pulled-right">
-                <FontIcon className="material-icons helpIcon" color={tooltipIconColor}>help_outline</FontIcon>
-            </IconButton>
-        </div>
+        <IconButton tooltip={tooltip} tooltipPosition="bottom-left" onClick={setDrawerContent} style={style}>
+            <FontIcon className="material-icons helpIcon" color={tooltipIconColor}>help_outline</FontIcon>
+        </IconButton>
     );
 };
 
