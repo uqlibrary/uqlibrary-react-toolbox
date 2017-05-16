d1a342ebba07a90d67dc287197770d4f09a08ead
Jan-Willem Wisgerhof <jwwisgerhof@gmail.com>
2017-05-16 13:39:48 +1000

    Buidl changes

--- build/HelpDrawer/components/HelpIcon.js
+++ build/HelpDrawer/components/HelpIcon.js
@@ -4,8 +4,6 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
-
 var _react = require('react');
 
 var _react2 = _interopRequireDefault(_react);
@@ -31,12 +29,10 @@ var propTypes = {
     tooltip: _propTypes2.default.string,
     tooltipIconColor: _propTypes2.default.string,
     onClick: _propTypes2.default.func,
-    inline: _propTypes2.default.bool,
     style: _propTypes2.default.object
 };
 
 var defaultProps = {
-    inline: false,
     style: {},
     tooltip: 'Click for more information',
     tooltipIconColor: '#CCCCCC'
@@ -49,26 +45,19 @@ var HelpIcon = function HelpIcon(_ref) {
         tooltip = _ref.tooltip,
         tooltipIconColor = _ref.tooltipIconColor,
         onClick = _ref.onClick,
-        inline = _ref.inline,
         style = _ref.style;
 
     var setDrawerContent = function setDrawerContent() {
         onClick(title, text, buttonLabel);
     };
 
-    var classNames = 'form-help-icon is-narrow is-pulled right ' + (inline ? 'inline' : '');
-
     return _react2.default.createElement(
-        'div',
-        { className: classNames, style: _extends({}, style) },
+        _IconButton2.default,
+        { tooltip: tooltip, tooltipPosition: 'bottom-left', onClick: setDrawerContent, style: style },
         _react2.default.createElement(
-            _IconButton2.default,
-            { tooltip: tooltip, tooltipPosition: 'bottom-left', onClick: setDrawerContent, className: 'is-pulled-right' },
-            _react2.default.createElement(
-                _FontIcon2.default,
-                { className: 'material-icons helpIcon', color: tooltipIconColor },
-                'help_outline'
-            )
+            _FontIcon2.default,
+            { className: 'material-icons helpIcon', color: tooltipIconColor },
+            'help_outline'
         )
     );
 };
