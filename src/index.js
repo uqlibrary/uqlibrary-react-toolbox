0102c45ad1c39711df2e4af16b05920906be35d6
uqadient <a.diente@library.uq.edu.au>
2017-06-12 15:12:24 +1000

    export the Toggle

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ export {HelpIcon, HelpDrawer, helpDrawerReducer} from './HelpDrawer';
 export {MenuDrawer} from './MenuDrawer';
 export {DatePicker} from './DatePicker';
 export {TextField} from './TextField';
+export {Toggle} from './Toggle';
 export {StaticPage} from './StaticPage';
 export {AppLoader} from './Loaders';
 export {InlineLoader} from './Loaders';
