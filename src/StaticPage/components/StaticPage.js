9d1c0a6b97d5952d51f422a9c33d9e426ae20402
Ky <k.lane@uq.edu.au>
2017-07-12 14:32:21 +1000

    Fixing StaticPage titles

--- src/StaticPage/components/StaticPage.js
+++ src/StaticPage/components/StaticPage.js
@@ -9,14 +9,14 @@ export default function StaticPage({title, text, help}) {
 
         <div className="layout-fill">
 
-            <span className="title is-2">{title ? title : 'This is the page title'}</span>
+            <h1 className="title is-2">{title ? title : 'This is the page title'}</h1>
 
             <Card className="layout-card">
                 <CardHeader className="card-header">
                     <div className="columns is-gapless is-mobile">
 
                         <div className="column">
-                            <span className="title is-3">{title ? title : 'This is the card title'}</span>
+                            <h2 className="title is-3">{title ? title : 'This is the card title'}</h2>
                         </div>
 
                         <div className="column is-narrow is-helpicon">
