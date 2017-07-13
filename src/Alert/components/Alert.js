078c55e29418edbc48cb7e45746c5e4821d8ed70
Ky <k.lane@uq.edu.au>
2017-07-13 16:39:50 +1000

    Refactored Alert per Janna

--- src/Alert/components/Alert.js
+++ src/Alert/components/Alert.js
@@ -2,32 +2,24 @@ import React from 'react';
 import {PropTypes} from 'prop-types';
 import FontIcon from 'material-ui/FontIcon';
 
-class Alert extends React.PureComponent {
-
-    static propTypes = {
-        alertText: PropTypes.string.isRequired,
-        alertType: PropTypes.string.isRequired,
-        alertState: PropTypes.string
-    };
-
-    render() {
-        const {alertType, alertText} = this.props;
-
-        // TODO: Get these valid types externally - they match with material icon names
-        const validAlertTypes = ['error', 'error_outline', 'warning', 'info', 'info_outline', 'help', 'help_outline'];
-        const validAlertType = validAlertTypes.find(type => type === alertType) || 'help';
-
+const Alert = ({title, message, type}) => {
         return (
-            <div className={validAlertType + ' alertWrapper columns'}>
+            <div className={type + ' alertWrapper columns'}>
                 <div className="column is-narrow alertIcon">
                     <FontIcon className="material-icons">{validAlertType}</FontIcon>
                 </div>
+                {title && <div className="column title is-4">{title}</div>}
                 <div className="column alertText">
-                    <div>{alertText}</div>
+                    <div>{message}</div>
                 </div>
             </div>
         );
     }
-}
+
+Alert.propTypes = {
+    message: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    type: PropTypes.oneOf(['error', 'error_outline', 'warning', 'info', 'info_outline', 'help', 'help_outline'])
+};
 
 export default Alert;
