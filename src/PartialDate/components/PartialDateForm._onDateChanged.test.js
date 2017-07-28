20051c9fb7c531fc965cbcb8e05dd9b716e0bde7
Vishal Asai <asai_vishal@yahoo.com>
2017-07-28 11:18:03 +1000

    more expectations for allowPartial date [#149208995]

--- src/PartialDate/components/PartialDateForm._onDateChanged.test.js
+++ src/PartialDate/components/PartialDateForm._onDateChanged.test.js
@@ -21,6 +21,26 @@ describe('PartialDateForm unit tests', () => {
 
     it('should return correct formatted date 2', () => {
         const expectToBe = (value) => {
+            expect(value).toBe(['', '2015-01-01', '', '2015-01-29', '', '2016-02-29', '2016-02-01'].shift());
+            return expectToBe;
+        };
+        const props = {
+            allowPartial: true,
+            onChange: (value) => (expectToBe)
+        };
+
+        const form = setup(props);
+        form.instance()._onDateChanged('year')({ target: { value: NaN }});
+        form.instance()._onDateChanged('year')({ target: { value: '2015' }});
+        form.instance()._onDateChanged('month')({ target: { value: undefined }}, 0, -1);
+        form.instance()._onDateChanged('day')({ target: { value: 29 }});
+        form.instance()._onDateChanged('month')({ target: { value: undefined }}, 2, 1);
+        form.instance()._onDateChanged('year')({ target: { value: '2016' }});
+        form.instance()._onDateChanged('day')({ target: { value: NaN }});
+    });
+
+    it('should return correct formatted date 3', () => {
+        const expectToBe = (value) => {
             expect(value).toBe(['', '', '', '', '', '20/09/2016'].shift());
             return expectToBe;
         };
