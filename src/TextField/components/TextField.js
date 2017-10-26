744e209d5489668091a342e2e0be45046e498c34
Ky Lane <k.lane@uq.edu.au>
2017-10-26 13:56:24 +1000

    Moved .dateField and .input-long-hint to toolbox, and added class props to TextField

--- src/TextField/components/TextField.js
+++ src/TextField/components/TextField.js
@@ -2,15 +2,15 @@ import React from 'react';
 import TextField from 'material-ui/TextField';
 import PropTypes from 'prop-types';
 
-
 import propFilter from '../../helpers/_filterProps';
 import {HelpIcon} from '../../HelpDrawer';
 
 const TextFieldWrapper = props => {
     const filteredProps = propFilter(props, TextField.propTypes);
+    const classProps = props.className ? props.className + ' input-long-hint' : 'input-long-hint';
     return (
         <div style={{position: 'relative', width: '100%'}}>
-            <TextField {...filteredProps} />
+            <TextField {...filteredProps} className={classProps} />
             {props.help && props.help.text && (
                 <HelpIcon {...props.help} />
             )}
