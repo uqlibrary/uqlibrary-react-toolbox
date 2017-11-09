c95c18b0562f91a031768fa3a02fa258d6f91b17
Ky Lane <k.lane@uq.edu.au>
2017-11-09 15:35:04 +1000

    [PT#144503469] Skip nav and refactored the way the main nav links

--- src/MenuDrawer/components/MenuDrawer.js
+++ src/MenuDrawer/components/MenuDrawer.js
@@ -8,7 +8,6 @@ import Drawer from 'material-ui/Drawer';
 import IconButton from 'material-ui/IconButton';
 import HardwareKeyboardArrowLeft from 'material-ui/svg-icons/hardware/keyboard-arrow-left';
 export default function MenuDrawer({menuItems, toggleDrawer, drawerOpen, docked, logoImage, logoText, history}) {
-
     const onNavigate = (to, target) => {
         if (to.indexOf('http://') === -1) {
             history.push(to);
