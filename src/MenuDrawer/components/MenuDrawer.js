2174e8f706f79f38f591ea494738a6e62d5a8b87
jannakha <j_malikova@yahoo.com>
2017-11-13 16:45:32 +1000

    added tests

--- src/MenuDrawer/components/MenuDrawer.js
+++ src/MenuDrawer/components/MenuDrawer.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import {withRouter} from 'react-router-dom';
-
+import {SkipNavigation} from './SkipNavigation';
 import {List, ListItem} from 'material-ui/List';
-import RaisedButton from 'material-ui/RaisedButton';
 import Divider from 'material-ui/Divider';
 import Drawer from 'material-ui/Drawer';
 import IconButton from 'material-ui/IconButton';
@@ -29,36 +27,16 @@ const MenuDrawer = ({menuItems, onToggleDrawer, drawerOpen, docked, logoImage,
     };
     const renderMenuItems = items => (
         items.map((menuItem, index) => (
-            menuItem.primaryText && menuItem.linkTo && (
-                <span className="menu-item-container" key={`menu_item_${index}`}>
-                    {
-                        menuItem.divider
-                            ? <Divider/>
-                            : <ListItem
-                                primaryText={menuItem.primaryText}
-                                secondaryText={menuItem.secondaryText}
-                                onTouchTap={navigateToLink.bind(this, menuItem.linkTo, menuItem.target)}
-                                leftIcon={menuItem.leftIcon ? menuItem.leftIcon : null} />
-                    }
+            menuItem.divider
+                ? <Divider key={`menu_item_${index}`}/>
+                : <span className="menu-item-container" key={`menu_item_${index}`}>
+                    <ListItem
+                        primaryText={menuItem.primaryText}
+                        secondaryText={menuItem.secondaryText}
+                        onTouchTap={navigateToLink.bind(this, menuItem.linkTo, menuItem.target)}
+                        leftIcon={menuItem.leftIcon ? menuItem.leftIcon : null}/>
                 </span>
-            )
         )));
-
-    const renderSkipNavigation = () => (
-        <div className="skipNav"
-            id="skipNav"
-            tabIndex={1}
-            onClick={skipMenuItems}
-            onKeyPress={skipMenuItems}
-            aria-label={locale.skipNavAriaLabel}>
-            <RaisedButton
-                secondary
-                className="skipNavButton"
-                label={locale.skipNavTitle}
-                onTouchTap={skipMenuItems}
-                tabIndex={-1}/>
-        </div>
-    );
 
     if (drawerOpen && !docked) {
         // set focus on menu on mobile view if menu is opened
@@ -72,29 +50,33 @@ const MenuDrawer = ({menuItems, onToggleDrawer, drawerOpen, docked, logoImage,
             width={320}
             onRequestChange={onToggleDrawer}
             docked={docked}>
-            <div className="layout-fill side-drawer">
-                <div className="logo-wrapper">
-                    <div className="columns is-gapless is-mobile">
-                        <div className="column is-centered">
-                            {logoImage && <img src={logoImage} alt={logoText}/>}
-                        </div>
-                        <div className="column is-narrow is-hidden-tablet menuCloseButton">
-                            <IconButton onTouchTap={onToggleDrawer} aria-label={locale.closeMenuLabel}>
-                                <HardwareKeyboardArrowLeft/>
-                            </IconButton>
+            {
+                drawerOpen &&
+                <div className="layout-fill side-drawer">
+                    <div className="logo-wrapper">
+                        <div className="columns is-gapless is-mobile">
+                            <div className="column is-centered">
+                                {logoImage && <img src={logoImage} alt={logoText}/>}
+                            </div>
+                            <div className="column is-narrow is-hidden-tablet menuCloseButton">
+                                <IconButton onTouchTap={onToggleDrawer} aria-label={locale.closeMenuLabel}>
+                                    <HardwareKeyboardArrowLeft/>
+                                </IconButton>
+                            </div>
                         </div>
                     </div>
+                    <List className="main-menu" id="mainMenu" tabIndex={-1}>
+                        {
+                            docked &&
+                            <SkipNavigation onClick={skipMenuItems} locale={locale}/>
+                        }
+                        {
+                            renderMenuItems(menuItems)
+                        }
+                    </List>
+                    <div id="afterMenuDrawer" tabIndex={-1} />
                 </div>
-                <List className="main-menu" id="mainMenu" tabIndex={-1}>
-                    {
-                        docked && renderSkipNavigation()
-                    }
-                    {
-                        drawerOpen && renderMenuItems(menuItems)
-                    }
-                </List>
-            </div>
-            <div id="afterMenuDrawer" tabIndex={-1} />
+            }
         </Drawer>
     );
 };
@@ -114,14 +96,4 @@ MenuDrawer.propTypes = {
     })
 };
 
-MenuDrawer.defaultValues = {
-    locale: {
-        skipNavTitle: 'Skip navigation',
-        skipNavAriaLabel: 'Skip navigation',
-        closeMenuLabel: 'Close menu'
-    }
-};
-
-export default withRouter(MenuDrawer);
-
-
+export default MenuDrawer;
--- src/MenuDrawer/components/MenuDrawer.test.js
+++ src/MenuDrawer/components/MenuDrawer.test.js
@@ -1,12 +1,28 @@
-jest.dontMock('./MenuDrawer');
-
-import {shallow} from 'enzyme';
+import { mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import toJson from 'enzyme-to-json';
 import React from 'react';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import PropTypes from 'prop-types';
+import {Provider} from 'react-redux';
+import Immutable from 'immutable';
 import MenuDrawer from './MenuDrawer';
 
-function setup(drawerOpen, menuItems, docked, logoImage, logoText) {
+const create = () => {
+    const initialState = Immutable.Map();
 
+    const store = {
+        getState: jest.fn(() => (initialState)),
+        dispatch: jest.fn(),
+        subscribe: jest.fn()
+    };
+    const next = jest.fn();
+    const invoke = (action) => thunk(store)(next)(action);
+    return {store, next, invoke}
+};
+
+function setup({isShallow = true, ...props}) {
+    // {menuItems, onToggleDrawer, drawerOpen, docked, logoImage, logoText, history, locale}
     const defaultMenuItems = [
         {
             linkTo: '/',
@@ -22,59 +38,74 @@ function setup(drawerOpen, menuItems, docked, logoImage, logoText) {
             secondaryText: 'secondary text 1'
         }
     ];
-
-
-    const props = {
-        menuItems: menuItems ? menuItems : defaultMenuItems,
-        drawerOpen: drawerOpen,
-        docked: docked,
-        toggleDrawer: () => { },
-        logoImage,
-        logoText
+    const defaultLocale = {
+        skipNavTitle: 'Skip navigation',
+        skipNavAriaLabel: 'Skip navigation',
+        closeMenuLabel: 'Close menu'
+    };
+    const testProps = {
+        ...props,
+        menuItems: props.menuItems || defaultMenuItems,
+        onToggleDrawer: props.onToggleDrawer || jest.fn(),
+        history: props.history || {push: jest.fn()},
+        locale: props.locale || defaultLocale,
+        drawerOpen: props.drawerOpen || false,
+        docked: props.docked || false
     };
 
-    return shallow(<MenuDrawer {...props} />);
+    if (!isShallow) {
+        return mount(
+            <Provider store={create().store}>
+                <MenuDrawer {...testProps} />
+            </Provider>, {
+                context: {
+                    muiTheme: getMuiTheme()
+                },
+                childContextTypes: {
+                    muiTheme: PropTypes.object.isRequired
+                }
+            });
+    }
+
+    return shallow(<MenuDrawer {...testProps} />);
 }
 
 describe('MenuDrawer snapshots tests', () => {
-    it('renders menu with dividers', () => {
-        const wrapper = setup();
+    it('should render empty drawer', () => {
+        const wrapper = setup({});
         const tree = toJson(wrapper);
         expect(tree).toMatchSnapshot();
     });
-    it('renders menu without dividers', () => {
-        const menuItems = [
-            {
-                linkTo: '/',
-                primaryText: 'Primary text 0',
-                secondaryText: 'secondary text 0'
-            },
-            {
-                linkTo: '/abc',
-                primaryText: 'Primary text 0',
-                secondaryText: 'secondary text 0'
-            }
-        ];
-        const wrapper = setup(false, menuItems, false);
+
+    it('should render opened drawer with menus, divider', () => {
+        const wrapper = setup({drawerOpen: true});
         const tree = toJson(wrapper);
         expect(tree).toMatchSnapshot();
     });
-    it('renders menu with logo', () => {
-        const menuItems = [
-            {
-                linkTo: '/',
-                primaryText: 'Primary text 0',
-                secondaryText: 'secondary text 0'
-            },
-            {
-                linkTo: '/abc',
-                primaryText: 'Primary text 0',
-                secondaryText: 'secondary text 0'
-            }
-        ];
-        const wrapper = setup(false, menuItems, false, 'http://image/image.svg', 'desc of image');
+
+    it('should render opened drawer with menus, divider and skip nav button', () => {
+        const wrapper = setup({drawerOpen: true, docked: true});
         const tree = toJson(wrapper);
         expect(tree).toMatchSnapshot();
+    });
+
+    it('should navigate to internal route', () => {
+        const testMethod = jest.fn();
+        const wrapper = setup({drawerOpen: true, docked: true, history: {push: testMethod}});
+        // navigateToLink
+        wrapper.find('span.menu-item-container ListItem').forEach(listItem => {
+            listItem.props().onTouchTap();
+            expect(testMethod).toHaveBeenCalled();
+        });
+    });
+
+    it('should skip navigation', () => {
+        const testMethod = jest.fn();
+        const wrapper = setup({drawerOpen: true, docked: true, history: {push: testMethod}});
+        // TODO: how to spy?
+        // const spy = jest.spyOn(wrapper.find('div#afterMenuDrawer'), 'focus');
+        wrapper.find('SkipNavigation').props().onClick();
+        // expect(spy).toHaveBeenCalled();
     });
 });
 
