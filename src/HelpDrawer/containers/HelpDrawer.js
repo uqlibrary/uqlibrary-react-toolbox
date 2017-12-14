9a009f7485b478bad8ce00243206ca33370019f6
jannakha <j_malikova@yahoo.com>
2017-12-14 18:06:06 +1000

    fixed empty map state to props

--- src/HelpDrawer/containers/HelpDrawer.js
+++ src/HelpDrawer/containers/HelpDrawer.js
@@ -4,7 +4,7 @@ import {hide} from '../actions';
 import HelpDrawer from '../components/HelpDrawer';
 
 const HelpDrawerContainer = connect(state => {
-    return state.get('helpDrawer').toJS();
+    return state.get('helpDrawer') ? state.get('helpDrawer').toJS() : {};
 }, dispatch => {
     return {
         hide: () => dispatch(hide())
