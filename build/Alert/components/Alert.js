6a77cb3b00b73883036d95555fb84133d884d496
Ky Lane <k.lane@uq.edu.au>
2018-02-27 15:26:10 +1000

    [PT#155542401][PT#155347979] Test of new showLoader for Alert and replaced FontIcons for SVGIcons

--- build/Alert/components/Alert.js
+++ build/Alert/components/Alert.js
@@ -12,10 +12,6 @@ var _react2 = _interopRequireDefault(_react);
 
 var _propTypes = require('prop-types');
 
-var _FontIcon = require('material-ui/FontIcon');
-
-var _FontIcon2 = _interopRequireDefault(_FontIcon);
-
 var _FlatButton = require('material-ui/FlatButton');
 
 var _FlatButton2 = _interopRequireDefault(_FlatButton);
@@ -24,9 +20,45 @@ var _IconButton = require('material-ui/IconButton');
 
 var _IconButton2 = _interopRequireDefault(_IconButton);
 
+var _CircularProgress = require('material-ui/CircularProgress');
+
+var _CircularProgress2 = _interopRequireDefault(_CircularProgress);
+
 var _close = require('material-ui/svg-icons/navigation/close');
 
 var _close2 = _interopRequireDefault(_close);
+
+var _error = require('material-ui/svg-icons/alert/error');
+
+var _error2 = _interopRequireDefault(_error);
+
+var _errorOutline = require('material-ui/svg-icons/alert/error-outline');
+
+var _errorOutline2 = _interopRequireDefault(_errorOutline);
+
+var _warning = require('material-ui/svg-icons/alert/warning');
+
+var _warning2 = _interopRequireDefault(_warning);
+
+var _info = require('material-ui/svg-icons/action/info');
+
+var _info2 = _interopRequireDefault(_info);
+
+var _infoOutline = require('material-ui/svg-icons/action/info-outline');
+
+var _infoOutline2 = _interopRequireDefault(_infoOutline);
+
+var _help = require('material-ui/svg-icons/action/help');
+
+var _help2 = _interopRequireDefault(_help);
+
+var _helpOutline = require('material-ui/svg-icons/action/help-outline');
+
+var _helpOutline2 = _interopRequireDefault(_helpOutline);
+
+var _done = require('material-ui/svg-icons/action/done');
+
+var _done2 = _interopRequireDefault(_done);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -36,7 +68,38 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var _ref = _react2.default.createElement(_close2.default, { className: 'alertDismiss' });
+// Icons
+// close
+// 'error'
+// 'error_outline'
+// 'warning'
+// 'info'
+// 'info_outline'
+// 'help'
+// 'help'
+
+
+// 'done'
+
+var _ref = _react2.default.createElement(_help2.default, { className: 'material-icons' });
+
+var _ref2 = _react2.default.createElement(_helpOutline2.default, { className: 'material-icons' });
+
+var _ref3 = _react2.default.createElement(_error2.default, { className: 'material-icons' });
+
+var _ref4 = _react2.default.createElement(_errorOutline2.default, { className: 'material-icons' });
+
+var _ref5 = _react2.default.createElement(_warning2.default, { className: 'material-icons' });
+
+var _ref6 = _react2.default.createElement(_info2.default, { className: 'material-icons' });
+
+var _ref7 = _react2.default.createElement(_infoOutline2.default, { className: 'material-icons' });
+
+var _ref8 = _react2.default.createElement(_done2.default, { className: 'material-icons' });
+
+var _ref9 = _react2.default.createElement(_CircularProgress2.default, { className: 'alertSpinner', size: 32, thickness: 4 });
+
+var _ref10 = _react2.default.createElement(_close2.default, { className: 'alertDismiss' });
 
 var Alert = function (_PureComponent) {
     _inherits(Alert, _PureComponent);
@@ -53,6 +116,45 @@ var Alert = function (_PureComponent) {
             return nextProps.message !== this.props.message || nextProps.title !== this.props.title || nextProps.type !== this.props.type || nextProps.action !== this.props.action || nextProps.actionButtonLabel !== this.props.actionButtonLabel || nextProps.allowDismiss !== this.props.allowDismiss || nextProps.dismissAction !== this.props.dismissAction;
         }
     }, {
+        key: '_getIcon',
+        value: function _getIcon(type, showLoader) {
+            console.log(type + showLoader);
+            var thisIcon = null;
+            if (!showLoader) {
+                switch (type) {
+                    case 'help':
+                        thisIcon = _ref;
+                        break;
+                    case 'help_outline':
+                        thisIcon = _ref2;
+                        break;
+                    case 'error':
+                        thisIcon = _ref3;
+                        break;
+                    case 'error_outline':
+                        thisIcon = _ref4;
+                        break;
+                    case 'warning':
+                        thisIcon = _ref5;
+                        break;
+                    case 'info':
+                        thisIcon = _ref6;
+                        break;
+                    case 'info_outline':
+                        thisIcon = _ref7;
+                        break;
+                    case 'done':
+                        thisIcon = _ref8;
+                        break;
+                    default:
+                        break;
+                }
+            } else {
+                thisIcon = _ref9;
+            }
+            return thisIcon;
+        }
+    }, {
         key: 'render',
         value: function render() {
             return _react2.default.createElement(
@@ -65,11 +167,7 @@ var Alert = function (_PureComponent) {
                         'div',
                         { className: 'column is-narrow alertIcon' + (this.props.action ? ' linked' : ''), onClick: this.props.action,
                             onKeyDown: this.props.action },
-                        _react2.default.createElement(
-                            _FontIcon2.default,
-                            { className: 'material-icons' },
-                            this.props.type
-                        )
+                        this._getIcon(this.props.type, this.props.showLoader)
                     ),
                     _react2.default.createElement(
                         'div',
@@ -102,7 +200,7 @@ var Alert = function (_PureComponent) {
                         _react2.default.createElement(
                             _IconButton2.default,
                             { onTouchTap: this.props.dismissAction, className: 'alertDismissButton' },
-                            _ref
+                            _ref10
                         )
                     )
                 )
@@ -117,6 +215,7 @@ Alert.defaultProps = {
     message: 'Unexpected error',
     title: 'Error',
     type: 'error',
-    allowDismiss: false
+    allowDismiss: false,
+    showLoader: false
 };
 exports.default = Alert;
--- src/Alert/components/Alert.js
+++ src/Alert/components/Alert.js
@@ -1,9 +1,19 @@
 import React, {PureComponent} from 'react';
 import {PropTypes} from 'prop-types';
-import FontIcon from 'material-ui/FontIcon';
 import FlatButton from 'material-ui/FlatButton';
 import IconButton from 'material-ui/IconButton';
-import NavigationClose from 'material-ui/svg-icons/navigation/close';
+import CircularProgress from 'material-ui/CircularProgress';
+
+// Icons
+import NavigationClose from 'material-ui/svg-icons/navigation/close'; // close
+import AlertError from 'material-ui/svg-icons/alert/error'; // 'error'
+import AlertErrorOutline from 'material-ui/svg-icons/alert/error-outline'; // 'error_outline'
+import AlertWarning from 'material-ui/svg-icons/alert/warning'; // 'warning'
+import ActionInfo from 'material-ui/svg-icons/action/info'; // 'info'
+import ActionInfoOutline from 'material-ui/svg-icons/action/info-outline'; // 'info_outline'
+import ActionHelp from 'material-ui/svg-icons/action/help'; // 'help'
+import ActionHelpOutline from 'material-ui/svg-icons/action/help-outline'; // 'help'
+import ActionDone from 'material-ui/svg-icons/action/done'; // 'done'
 
 export default class Alert extends PureComponent {
     static propTypes = {
@@ -13,14 +23,16 @@ export default class Alert extends PureComponent {
         action: PropTypes.func,
         actionButtonLabel: PropTypes.string,
         allowDismiss: PropTypes.bool,
-        dismissAction: PropTypes.func
+        dismissAction: PropTypes.func,
+        showLoader: PropTypes.bool
     };
 
     static defaultProps = {
         message: 'Unexpected error',
         title: 'Error',
         type: 'error',
-        allowDismiss: false
+        allowDismiss: false,
+        showLoader: false
     };
 
     constructor(props) {
@@ -37,13 +49,51 @@ export default class Alert extends PureComponent {
             || nextProps.dismissAction !== this.props.dismissAction;
     }
 
+    _getIcon(type, showLoader) {
+        console.log(type + showLoader);
+        let thisIcon = null;
+        if(!showLoader) {
+            switch(type) {
+                case 'help':
+                    thisIcon = <ActionHelp className="material-icons" />;
+                    break;
+                case 'help_outline':
+                    thisIcon = <ActionHelpOutline className="material-icons" />;
+                    break;
+                case 'error':
+                    thisIcon = <AlertError className="material-icons" />;
+                    break;
+                case 'error_outline':
+                    thisIcon = <AlertErrorOutline className="material-icons" />;
+                    break;
+                case 'warning':
+                    thisIcon = <AlertWarning className="material-icons" />;
+                    break;
+                case 'info':
+                    thisIcon = <ActionInfo className="material-icons" />;
+                    break;
+                case 'info_outline':
+                    thisIcon = <ActionInfoOutline className="material-icons" />;
+                    break;
+                case 'done':
+                    thisIcon = <ActionDone className="material-icons" />;
+                    break;
+                default:
+                    break;
+            }
+        } else {
+            thisIcon = <CircularProgress className="alertSpinner" size={32} thickness={4} />;
+        }
+        return thisIcon;
+    }
+
     render() {
         return (
             <div className={this.props.type + ' alertWrapper '}>
                 <div className="columns is-multiline is-mobile">
                     <div className={`column is-narrow alertIcon${this.props.action ? ' linked' : ''}`} onClick={this.props.action}
                         onKeyDown={this.props.action}>
-                        <FontIcon className="material-icons">{this.props.type}</FontIcon>
+                        {this._getIcon(this.props.type, this.props.showLoader)}
                     </div>
                     <div className={`column alertText${this.props.action ? ' linked' : ''}`} onClick={this.props.action} onKeyDown={this.props.action}>
                         <div><b>{this.props.title}</b>&nbsp;-&nbsp;{this.props.message}</div>
