7578b722e7dd52e7775551c506f65a4164523d3e
Vishal Asai <asai_vishal@yahoo.com>
2018-03-08 15:50:25 +1000

    updates access selector tests [#155587161]

--- src/FileUploader/components/FileUploadAccessSelector.snapshot.test.js
+++ src/FileUploader/components/FileUploadAccessSelector.snapshot.test.js
@@ -28,21 +28,37 @@ const defaultConfig = {
 
 describe('Component FileUploadAccessSelector', () => {
     it('should render with default setup', () => {
-        const onAccessChangedTestFn = jest.fn();
-        const props = {locale, defaultConfig, onAccessChanged: onAccessChangedTestFn};
+        const onChangeTestFn = jest.fn();
+        const props = {locale, defaultConfig, onChange: onChangeTestFn};
         const wrapper = setup({...props});
         expect(toJson(wrapper)).toMatchSnapshot();
 
         wrapper.instance()._onChange({}, 0, 2);
         wrapper.update();
 
-        expect(onAccessChangedTestFn).toHaveBeenCalledWith({key: 'access_condition_id', value: 2});
-        expect(wrapper.state().value).toEqual(2);
+        expect(onChangeTestFn).toHaveBeenCalledWith(2);
+    });
+
+    it('should render with value', () => {
+        const onChangeTestFn = jest.fn();
+        const props = {locale, defaultConfig, onChange: onChangeTestFn, value: 1};
+        const wrapper = setup({...props});
         expect(toJson(wrapper)).toMatchSnapshot();
+
+        wrapper.instance()._onChange({}, 0, 2);
+        wrapper.update();
+
+        expect(onChangeTestFn).toHaveBeenCalledWith(2);
     });
 
     it('should render access condition disabled if disabled flag is set', () => {
         const props = {locale, defaultConfig, disabled: true};
+        const wrapper = setup({...props}, false);
+        expect(toJson(wrapper)).toMatchSnapshot();
+    });
+
+    it('should render access condition disabled if disabled flag is set and with value', () => {
+        const props = {locale, defaultConfig, disabled: true, value: 2};
         const wrapper = setup({...props}, false);
         expect(toJson(wrapper)).toMatchSnapshot();
     });
